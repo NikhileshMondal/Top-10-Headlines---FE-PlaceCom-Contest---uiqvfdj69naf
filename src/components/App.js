109821202a890ce519f7ed65bf128a64621028a9
NikhileshMondal <109812970+NikhileshMondal@users.noreply.github.com>
2023-04-01 20:24:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,35 @@
 import React, { useState, useEffect } from 'react'
 import '../styles/App.css';
 
+const posts= (category)=>{
+  const API_KEY="a1741280db6dbc554c889a31544851b7"
+  let url=`https://gnews.io/api/v4/top-headlines?category=${category}&apikey=${API_KEY}&max=10&lang=en`;
+  return fetch(url)
+}
+
 const App = () => {
+  
   const [category, setCategory] = useState("general");
   const [newsData, setNewsData] = useState([]);
   const [loading, setLoading] = useState(true);
- const API_KEY="a1741280db6dbc554c889a31544851b7";
 
- const changeFunction=(e)=>
- {
-  setCategory(e.target.value);
- }
- useEffect(()=>
- {
-   setLoading(true);
-   fetch(`https://gnews.io/api/v4/top-headlines?category=${category}&lang=en&country=us&max=10&apikey=${API_KEY}`).then(res=>res.json())
-   .then((res)=>{
-    setNewsData(res.articles)
-    console.log(res);
-  }).then(()=>setLoading(false));
- },[category])
+  useEffect(()=>{
+    setLoading(true)
+    posts(category)
+    .then((res)=>res.json())
+    .then((data)=>{
+      setNewsData(data.articles)
+    })
+    .then(()=>{
+      setLoading(false)
+    })
+    console.log(newsData)
+  },[category])
+
   return (
     <div id="main">
       <h1 className='heading'>Top 10 {category} news.</h1>
-      <select value={category} onChange={changeFunction}>
+      <select value={category} onChange={(e)=>{setCategory(e.target.value)}}>
         <option value="general">General</option>
         <option value="business">Business</option>
         <option value="sports">Sports</option>
@@ -33,21 +39,21 @@ const App = () => {
         <option value="science">Science</option>
       </select>
       {loading && <p className='loader'>Loading...</p>}
-      {!loading &&<ol>
-       { newsData.map((e,i)=>
-        {
-          return (<li key={i}>
-          <img className='news-img' src={e.image} alt=""/>
+      {!loading &&
+      <ol>
+        {newsData.map((item,index)=>{
+          return(
+        <li key={index}>
+          <img className='news-img' src={item.image} alt="randomimg"/>
           <section className='new-title-content-author'>
-            <h3 className='news-title'>{e.title}</h3>
+            <h3 className='news-title'>{item.title}</h3>
             <section className='new-content-author'>
-              <p className='news-description'>{e.description}</p>
-              <p className='news-source'><strong>Source:</strong>{e.source.name}</p>
+              <p className='news-description'>{item.description}</p>
+              <p className='news-source'><strong>Source:</strong>{item.source.name}</p>
             </section>
           </section>
         </li>)
-        })
-      }
+        })}
       </ol>}
     </div>
   )
